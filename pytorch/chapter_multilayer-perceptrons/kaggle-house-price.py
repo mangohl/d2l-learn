ca95d5d450059fc6b410b1f56a270c5853f4df26
hua <hua>
2024-10-15 17:47:50 +0800

    add kaggle-house-price

--- pytorch/chapter_multilayer-perceptrons/kaggle-house-price.py
+++ pytorch/chapter_multilayer-perceptrons/kaggle-house-price.py
@@ -16,7 +16,7 @@ DATA_URL = 'http://d2l-data.s3-accelerate.amazonaws.com/'
 
 #url.split('/')[-1] get the filename
 #如果文件存在其hash匹配就不用再下载了
-def download(name,cache_dir=os.path.jon('..','data')):
+def download(name,cache_dir=os.path.join('..','data')):
     assert name in DATA_HUB ,f"{name} not exist in {DATA_HUB}"
     url,sha1_hash = DATA_HUB[name]
     os.makedirs(cache_dir,exist_ok=True)
@@ -71,19 +71,21 @@ test_data = pd.read_csv(download('kaggle_house_test'))
 
 #1:-1 从第一列到倒数第1列
 #all_features包含了所有样本的特征信息
-all_features = pd.concat((train_data.iloc[:,1:-1],test_data.iloc[:,1:]))
+all_features = pd.concat((train_data.iloc[:,1:],test_data.iloc[:,1:]))
 
 #all_features的shape?
 #先返回所有类型是数值的特征所在的所有列
 #numric_features类型是pandas中Index对象，这里可以理解为列名称
-#all_features[]是按列操作
+#all_features[]是按列名进行操作
 #该步骤是对所有数值特征作归一化预处理
-numeric_features = all_features.dyptes[all_features.dtypes != 'object'].index
+numeric_features = all_features.dtypes[all_features.dtypes != 'object'].index
 all_features[numeric_features] = all_features[numeric_features].apply(
     lambda x: (x - x.mean()) / (x.std()))
+all_features[numeric_features] = all_features[numeric_features].fillna(0)
+
 
 #将object类型(比如字符串)数据进行独热编码
-all_features = pd.get_dummies(all_features, dummy_na=True)
+all_features = pd.get_dummies(all_features, dummy_na=True,dtype=int)
 all_features.shape
 
 
@@ -108,3 +110,68 @@ def log_rmse(net, features, labels):
     rmse = torch.sqrt(loss(torch.log(clipped_preds),
                            torch.log(labels)))
     return rmse.item()
+
+#nn.MSELoss返回的是均方误差，是求过平均的了，所以不用l.mean().backward()
+def train(net,train_features,train_labels,test_features,test_labels,
+          num_epochs,lr,weight_decay,batch_size):
+    train_ls,test_ls = [],[]
+    train_iter = d2l.load_array((train_features,train_labels),batch_size)
+    optimizer = torch.optim.Adam(net.parameters(),
+                                 lr=lr,
+                                 weight_decay=weight_decay)
+    for epoch in range(num_epochs):
+        for X,y in train_iter:
+            optimizer.zero_grad()
+            l=loss(net(X),y)
+            l.backward()
+            optimizer.step()
+        train_ls.append(log_rmse(net,train_features,train_labels))
+        if test_labels is not None:
+            test_ls.append(log_rmse(net, test_features, test_labels))
+    return train_ls, test_ls
+
+
+#将X分为K个部分，每个部分大小为X.shape[0] /k，并指定第i部分为验证集
+#不够整除时，会导致一部分样本丢失
+#k这验证会使用一折作用验证集合，其余的作为训练集
+#每次train使用的验证集是train集的0-k中的其中一折
+def get_k_fold_data(k, i, X, y):
+    assert k > 1
+    fold_size = X.shape[0] // k
+    X_train, y_train = None, None
+    for j in range(k):
+        idx = slice(j * fold_size, (j + 1) * fold_size)
+        X_part, y_part = X[idx, :], y[idx]
+        if j == i:
+            X_valid, y_valid = X_part, y_part
+        elif X_train is None:
+            X_train, y_train = X_part, y_part
+        else:
+            X_train = torch.cat([X_train, X_part], 0)
+            y_train = torch.cat([y_train, y_part], 0)
+    return X_train, y_train, X_valid, y_valid
+
+#train_ls[-1] get the last value in train_ls
+#这里对利用所有样本一共训练了k*epochs次，每次train训练epochs次数
+def k_fold(k,X_train,y_train,num_epochs,lr,weight_decay,batch_size):
+    train_l_sum,valid_l_sum =0,0
+    for i in range(k):
+        data = get_k_fold_data(k,i,X_train,y_train)
+        net = get_net()
+        train_ls,valid_ls = train(net,*data,num_epochs,lr,weight_decay,batch_size)
+        train_l_sum += train_ls[-1]
+        valid_l_sum += valid_ls[-1]
+        if i == 0:
+            d2l.plot(list(range(1, num_epochs + 1)), [train_ls, valid_ls],
+                     xlabel='epoch', ylabel='rmse', xlim=[1, num_epochs],
+                     legend=['train', 'valid'], yscale='log')
+        print(f'折{i + 1}，训练log rmse{float(train_ls[-1]):f}, '
+              f'验证log rmse{float(valid_ls[-1]):f}')
+    return train_l_sum / k, valid_l_sum / k
+
+
+k, num_epochs, lr, weight_decay, batch_size = 5, 100, 5, 0, 64
+train_l, valid_l = k_fold(k, train_features, train_labels, num_epochs, lr,
+                          weight_decay, batch_size)
+print(f'{k}-折验证: 平均训练log rmse: {float(train_l):f}, '
+      f'平均验证log rmse: {float(valid_l):f}')
